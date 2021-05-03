06914fe9f9b5ad2be5d2a24b8e7b80c26e17f0d0
jokohoko <jokohoko13@hotmail.com>
2021-05-03 18:39:28 +0200

    Big update
    Changed the sensor to Serial data mode,
    Made two sound levels possible,
    More consistent chirps,
    LED output is without bugs now, ...

--- arduino.ino
+++ arduino.ino
@@ -1,31 +1,46 @@
-#include <MHZ19PWM.h>
+#include <MHZ19.h>
+MHZ19 mhz(&Serial1);
 #include <Adafruit_NeoPixel.h>
 
-#define mhzpin 10
 #define piezopin 9
-#define spiezopin 6
 #define pixelpin 8
-boolean loud = true;
+#define serial_rx 7
+#define serial_tx 6
+#define spiezopin 5
+MHZ19_RESULT response;
 
-MHZ19PWM mhz(mhzpin, MHZ_CONTINUOUS_MODE);
 Adafruit_NeoPixel pixel(1, pixelpin, NEO_GRBW + NEO_KHZ800);
 
-void setup(){
-  pinMode(piezopin, OUTPUT);
+void setup() {
   pixel.begin();
   pixel.clear();
-  pixel.setBrightness(200);
+  pixel.setBrightness(150);
   Serial.begin(115200);
-  mhz.useLimit(5000);
-  chirp();
-  loud = true;
+  Serial1.begin(9600);
+  pixel.setPixelColor(0, pixel.Color(0, 0, 0, 100)); pixel.show();
+  chirp(HIGH);
+  response = mhz.retrieveData();
+}
+
+void loud_mode() {
+  pinMode(piezopin, OUTPUT);
+  pinMode(spiezopin, INPUT);
+}
+void silent_mode() {
+  pinMode(piezopin, INPUT);
+  pinMode(spiezopin, OUTPUT);
 }
 
 void loop() {
-  for (int t = 0; t < random(10, 200); t++) {
-    //takes 2sec to get a reading
-    float co2 = mhz.getCO2();
-    delay(2000);
+  for (int t = 0; t < random(30, 300); t++) {
+    // We take a reading every sec
+    response = mhz.retrieveData();
+    while (response != MHZ19_RESULT_OK) {
+      pixel.setPixelColor(0, pixel.Color(0, 0, 255)); pixel.show();
+      delay(100);
+      response = mhz.retrieveData();
+    }
+    int co2 = mhz.getCO2();
     Serial.print(F("CO2: "));
     Serial.print(co2);
 
@@ -33,40 +48,45 @@ void loop() {
     int G = map(co2, 200, 2000, 255, 0);
     R = constrain(R, 0, 255);
     G = constrain(G, 0, 255);
-    pixel.setPixelColor(0, pixel.Color(R, G, 0));
-    pixel.show();
+    pixel.setPixelColor(0, pixel.Color(R, G, 0)); pixel.show();
 
-    if (co2 > 2000) {
+    if (co2 > 1400) {
       //big alarm
       Serial.println(F(" That's horrible!"));
-      loud = true;
-      for (int l = 0; l < 200; l++) {
+      loud_mode();
+      for (int l = 0; l < 15; l++) {
         lowChirp(random(5, 10), 1);
-        if (random(0, 10) == 0) {
+        if (random(0, 5) == 0) {
           tweet(random(2, 12), 2);
         }
       }
-    } else if (co2 > 1000) {
+    } else if (co2 > 850) {
       //small alarm
       Serial.println(F(" That's not so good!"));
-      loud = true;
-      for (int l = 0; l < 100; l++) {
+      silent_mode();
+      for (int l = 0; l < 10; l++) {
         highChirp(random(5, 10), 1);
-        if (random(0, 10) == 0) {
+        if (random(0, 5) == 0) {
           tweet(random(2, 12), 2);
         }
       }
     } else if (co2 > 600) {
       Serial.println(F(" That is ok"));
+      delay(1000);
     } else {
       Serial.println(F(" That is great!"));
+      delay(1000);
     }
   }
-  loud = true;
-  chirp();
+  chirp(LOW);
 }
 
-void chirp() {
+void chirp(boolean volume) {
+  if (volume) {
+    loud_mode();
+  } else {
+    silent_mode();
+  }
   Serial.println("chirp");
   for (int i = 0; i < random(1, 3); i++ ) {
     highChirp(5, random(1, 5)); //intensity, amount of chirps
@@ -78,13 +98,14 @@ void chirp() {
 }
 
 void highChirp(int intensity, int chirpsNumber) {
-  int pin = loud?piezopin:spiezopin;
   for (int veces = 0; veces <= chirpsNumber; veces++) {
     for (int i = 100; i > 0; i--) {
       for  (int x = 0; x < intensity;  x++) {
-        digitalWrite (pin, HIGH);
+        digitalWrite (piezopin, HIGH);
+        digitalWrite (spiezopin, HIGH);
         delayMicroseconds (i);
-        digitalWrite (pin, LOW);
+        digitalWrite (piezopin, LOW);
+        digitalWrite (spiezopin, LOW);
         delayMicroseconds (i);
       }
     }
@@ -92,19 +113,22 @@ void highChirp(int intensity, int chirpsNumber) {
 }
 
 void lowChirp(int intensity, int chirpsNumber) {
-  int pin = loud?piezopin:spiezopin;
   for (int veces = 0; veces <= chirpsNumber; veces++) {
     for (int i = 0; i < 200; i++) {
-      digitalWrite (pin, HIGH);
-      delayMicroseconds(i);
-      digitalWrite(pin, LOW);
-      delayMicroseconds(i);
+      digitalWrite (piezopin, HIGH);
+      digitalWrite (spiezopin, HIGH);
+      delayMicroseconds (i);
+      digitalWrite (piezopin, LOW);
+      digitalWrite (spiezopin, LOW);
+      delayMicroseconds (i);
     }
     for (int i = 90; i > 80; i--) {
       for  (int x = 0; x < 5;  x++) {
-        digitalWrite (pin, HIGH);
+        digitalWrite (piezopin, HIGH);
+        digitalWrite (spiezopin, HIGH);
         delayMicroseconds (i);
-        digitalWrite (pin, LOW);
+        digitalWrite (piezopin, LOW);
+        digitalWrite (spiezopin, LOW);
         delayMicroseconds (i);
       }
     }
@@ -112,14 +136,15 @@ void lowChirp(int intensity, int chirpsNumber) {
 }
 
 void tweet(int intensity, int chirpsNumber) {
-  int pin = loud?piezopin:spiezopin;
   //normal chirpsNumber 3, normal intensity 5
   for (int veces = 0; veces < chirpsNumber; veces++) {
     for (int i = 80; i > 0; i--) {
       for  (int x = 0; x < intensity;  x++) {
-        digitalWrite (pin, HIGH);
+        digitalWrite (piezopin, HIGH);
+        digitalWrite (spiezopin, HIGH);
         delayMicroseconds (i);
-        digitalWrite (pin, LOW);
+        digitalWrite (piezopin, LOW);
+        digitalWrite (spiezopin, LOW);
         delayMicroseconds (i);
       }
     }
